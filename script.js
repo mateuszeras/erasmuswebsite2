2f951964ef4d55d2cc4e6aed6a9ac492c38e0d66
mateuszeras <mateusz.schule.eras@gmail.com>
2025-04-29 08:12:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function ShowEmailAddress(){
+    document.getElementById("htext").innerHTML = "<br>katja.schmidt3@lk.brandenburg.de <br> romy.thiel2@lk.brandenburg.de<br>stefan.knoche@lk.brandenburg.de";
+}
